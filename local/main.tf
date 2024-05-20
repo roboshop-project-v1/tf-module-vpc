f3bbaaea97eb94b0c6c5b5ef2e179b4669363e2a
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-20 23:59:03 +0530

    ok

--- local/main.tf
+++ local/main.tf
@@ -11,20 +11,17 @@ resource "aws_route_table" "main" {
   for_each = var.subnets_list
   vpc_id = var.vpc_id
 
-  route {
-    cidr_block = each.value["cidr"]
-  }
   tags = {
     name = each.key
   }
 
 }
 
-# resource "aws_route_table_association" "main" {
-#   for_each = var.subnets_list
-#   subnet_id      = lookup(aws_subnet.main,each.ke)
-#   route_table_id = aws_route_table.bar.id
-# }
+resource "aws_route_table_association" "main" {
+  for_each = var.subnets_list
+  subnet_id      = lookup(lookup(aws_subnet.main,each.key,null),"id",null)
+  route_table_id = lookup(lookup(aws_route_table.main,each.key,null),"id",null)
+}
 
 output "name" {
   value = aws_subnet.main
