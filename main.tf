287ce5490e900971812aa66d97a2daf08a0dc5ad
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-20 22:19:17 +0530

    okok

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
 }
+
+# resource "aws_subnet" "main" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.1.0/24"
+
+
+# }
+
+output "vpc_details" {
+    value = aws_vpc.main
+  
+}
