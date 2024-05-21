637b04ac12699f74cb10eb81a21509755e3f785f
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-22 01:23:24 +0530

    ok

--- main.tf
+++ main.tf
@@ -91,11 +91,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
 
 resource "aws_instance" "web" {
   ami           = "ami-0f3c7d07486cad139"
