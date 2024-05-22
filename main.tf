ad23d02050924a863544c266eb831526453f48f5
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-22 21:28:28 +0530

    kk

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.default_vpc_cidr
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
